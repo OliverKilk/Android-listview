3f7ff0bce51f70fda30dad2c6b0253bfd1a0a6c1
Eric <eric.talviste@tptlive.ee>
2018-03-09 09:52:48 +0200

    added code

---
+++ androidtest/androidtest/Resources/car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace androidtest.Resources
+{
+    class Car
+    {
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public string HP { get; set; }
+        public string Swag_o_meter { get; set; }
+    }
+}
